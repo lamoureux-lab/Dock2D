bf690a03722ab38dc178764ff7db13d93e304e07
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-05-21 07:38:50 -0400

    removed epoch level shuffle RandomSampler() for interaction, already shuffled in __init__()

--- Utility/TorchDataLoader.py
+++ Utility/TorchDataLoader.py
@@ -74,7 +74,7 @@ class ToyInteractionDataset(Dataset):
 		if not number_of_pairs:
 			max_size = len(self.data)
 		else:
-			max_size = int(number_of_pairs + (number_of_pairs**2 - number_of_pairs)/2)
+			max_size = int((number_of_pairs**2 + number_of_pairs)/2)
 
 		random.shuffle(self.data)
 		self.data = self.data[:max_size]
@@ -120,13 +120,18 @@ def get_interaction_stream(data_path, shuffle=False, number_of_pairs=None, num_w
 	:return: interaction data stream [receptor, ligand, 1 or 0] (see DatasetGeneration).
 	'''
 	dataset = ToyInteractionDataset(path=data_path, number_of_pairs=number_of_pairs)
-	sampler = RandomSampler(dataset)
-	trainloader = torch.utils.data.DataLoader(dataset, sampler=sampler, batch_size=1, num_workers=num_workers, shuffle=shuffle)
+	# sampler = RandomSampler(dataset)
+	trainloader = torch.utils.data.DataLoader(dataset, batch_size=1, num_workers=num_workers, shuffle=shuffle)
 	return trainloader
 
 
 if __name__=='__main__':
 	import timeit
+	from tqdm import tqdm
+	import matplotlib.pyplot as plt
+	import numpy as np
+	import seaborn as sea
+	sea.set_style("whitegrid")
 
 	train_datapath = '../Datasets/interaction_train_100pool.pkl'
 	valid_datapath = '../Datasets/interaction_valid_100pool.pkl'
@@ -134,8 +139,18 @@ if __name__=='__main__':
 
 	number_of_pairs = 100
 	start = timeit.default_timer()
-	get_interaction_stream(train_datapath, number_of_pairs=number_of_pairs)
-	get_interaction_stream(valid_datapath, number_of_pairs=number_of_pairs)
-	get_interaction_stream(test_datapath, number_of_pairs=number_of_pairs)
+	train_stream = get_interaction_stream(train_datapath, number_of_pairs=number_of_pairs)
+	valid_stream = get_interaction_stream(valid_datapath, number_of_pairs=number_of_pairs)
+	test_stream = get_interaction_stream(test_datapath, number_of_pairs=number_of_pairs)
 	end = timeit.default_timer()
 	print('time to load datasets', end-start)
+
+	for epoch in range(3):
+		counter = 0
+		for data in tqdm(train_stream):
+			receptor, ligand, interaction = data
+			if counter == 7:
+				plot = np.hstack((receptor.squeeze().detach().cpu(), ligand.squeeze().detach().cpu()))
+				plt.imshow(plot)
+				plt.show()
+			counter += 1
