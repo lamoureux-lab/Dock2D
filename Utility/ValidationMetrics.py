91d99799ac849dca0faaa9825862f923f84458f7
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-06-02 15:24:48 -0400

    replaced rmsd epsilon with if < 0 check

--- Utility/ValidationMetrics.py
+++ Utility/ValidationMetrics.py
@@ -70,15 +70,16 @@ class RMSD:
 
         I = torch.diag(torch.ones(2, dtype=torch.float64)).cuda()
         # RMSD
+        # RMSD
         rmsd = torch.sum(T * T)
         rmsd = rmsd + torch.sum((I - R) * X, dim=(0, 1))
-        rmsd = rmsd + 2.0 * torch.sum(torch.sum(T.unsqueeze(dim=1) * (R1 - R2), dim=0) * C, dim=0) + self.epsilon
-        # print(rmsd)
+        rmsd = rmsd + 2.0 * torch.sum(torch.sum(T.unsqueeze(dim=1) * (R1 - R2), dim=0) * C, dim=0)
+        if rmsd < 0:
+            rmsd = 0.0
+        else:
+            rmsd = torch.sqrt(rmsd)
 
-        # sqrt_rmsd = torch.sqrt(rmsd)
-        sqrt_rmsd = torch.sqrt(rmsd) - torch.sqrt(torch.tensor(self.epsilon))
-
-        return sqrt_rmsd
+        return rmsd
 
 
 class APR:
