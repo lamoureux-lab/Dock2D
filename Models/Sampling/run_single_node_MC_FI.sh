758d2d02edd60899116d6d1850d02844712ce9ad
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-08-01 16:58:29 -0400

    testing slurm headers

--- Models/Sampling/run_single_node_MC_FI.sh
+++ Models/Sampling/run_single_node_MC_FI.sh
@@ -4,15 +4,15 @@
 #SBATCH --nodes=1
 ##SBATCH --ntasks=1
 #SBATCH --tasks-per-node=1
-##SBATCH --cpus-per-task=12
-#SBATCH --gres=gpu:1 # Number of GPUs
-##SBATCH --constraint=volta
-##SBATCH --time=72:00:00
+#SBATCH --cpus-per-task=12
+#SBATCH --gres=gpu:2 # Number of GPUs
+#SBATCH --constraint=volta
+#SBATCH --time=72:00:00
 #SBATCH --output=slurm_log/slurm.%N.%j.%x.out
 #SBATCH --error=slurm_log/slurm.%N.%j.%x.err
 #SBATCH --export=ALL
 
 pwd
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+#export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 srun -N1 -n1 python train_montecarlo_FI.py;
