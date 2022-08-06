24c97227c57ec34cef5f3481cfdc08fcc4f20ff0
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2022-08-06 09:14:43 -0400

    resume at epoch133 p_ccib_1

--- Models/Sampling/train_montecarlo_FI.py
+++ Models/Sampling/train_montecarlo_FI.py
@@ -84,19 +84,19 @@ if __name__ == '__main__':
               FI_MC=True)
     ######################
     ### Train model from beginning
-    Trainer.run_trainer(train_epochs, train_stream=train_stream, valid_stream=None, test_stream=None)
+    # Trainer.run_trainer(train_epochs, train_stream=train_stream, valid_stream=None, test_stream=None)
     #
     ### resume training model
-    # Trainer.run_trainer(resume_training=True, resume_epoch=train_epochs, train_epochs=1,
-    #                                            train_stream=train_stream, valid_stream=None, test_stream=None)
+    Trainer.run_trainer(resume_training=True, resume_epoch=133, train_epochs=(1000-133),
+                                               train_stream=train_stream, valid_stream=None, test_stream=None)
     #
-    ### Evaluate model at chosen epoch (Brute force evaluation)
-    eval_angles = 360
-    evalFFT = TorchDockingFFT(padded_dim=padded_dim, num_angles=eval_angles)
-    eval_model = SamplingModel(evalFFT, FI_MC=True).to(device=0)
-    TrainerFI(eval_model, docking_optimizer, interaction_model, interaction_optimizer, experiment, FI_MC=True
-                                  ).run_trainer(resume_training=True, resume_epoch=1000, train_epochs=1,
-                                                train_stream=None, valid_stream=valid_stream, test_stream=test_stream)
+    # ### Evaluate model at chosen epoch (Brute force evaluation)
+    # eval_angles = 360
+    # evalFFT = TorchDockingFFT(padded_dim=padded_dim, num_angles=eval_angles)
+    # eval_model = SamplingModel(evalFFT, FI_MC=True).to(device=0)
+    # TrainerFI(eval_model, docking_optimizer, interaction_model, interaction_optimizer, experiment, FI_MC=True
+    #                               ).run_trainer(resume_training=True, resume_epoch=100, train_epochs=1,
+    #                                             train_stream=None, valid_stream=valid_stream, test_stream=test_stream)
 
     ### Plot loss and free energy distributions with learned F_0 decision threshold
     # PlotterFI(experiment).plot_loss(show=show)
