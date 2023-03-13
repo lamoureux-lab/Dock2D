a50ea4cdd4002af21c8051ca6e483a91431bb213
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2023-03-13 11:36:06 -0400

    conf.py path test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ import sphinx_rtd_theme
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('../../../'))
+sys.path.insert(0, os.path.abspath('../docs/'))
 # print(sys.executable)
 
 # -- Project information -----------------------------------------------------
