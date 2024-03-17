a9eb333ee79991970fead1db38d1bb54851ca68d
sidbhadra-lobo <sbhadralobo@ucdavis.edu>
2024-03-17 13:54:35 -0400

    profiler checked

--- Tests/run_pytorch_profiler.py
+++ Tests/run_pytorch_profiler.py
@@ -10,10 +10,10 @@ from Dock2D.Utility.TorchDockingFFT import TorchDockingFFT
 if __name__ == '__main__':
     #################################################################################
     # Datasets
-    trainset = '../Datasets/docking_train_400pool.pkl'
-    validset = '../Datasets/docking_valid_400pool.pkl'
+    trainset = '../Datasets/docking_train_50pool.pkl'
+    validset = '../Datasets/docking_valid_50pool.pkl'
     ### testing set
-    testset = '../Datasets/docking_test_400pool.pkl'
+    testset = '../Datasets/docking_test_50pool.pkl'
     #########################
     #### initialization of random seeds
     random_seed = 42
@@ -26,19 +26,24 @@ if __name__ == '__main__':
     # torch.autograd.set_detect_anomaly(True)
     ######################
     max_size = 10
+    # max_size = 1
     train_stream = get_docking_stream(trainset, max_size=max_size)
     # valid_stream = get_docking_stream(validset,  max_size=None, shuffle=False)
     # test_stream = get_docking_stream(testset, max_size=None, shuffle=False)
     ######################
-    experiment = 'BF_IP_profiler_check'
+    # experiment = 'BF_IP_profiler_check'
+    experiment = 'BS_IP_profiler_check'
+
     ######################
     plotting = False
 
-    train_epochs = 1
-    learning_rate = 10 ** -3
+    train_epochs = 100
+    # learning_rate = 10 ** -3
+    learning_rate = 10 ** -1
 
     padded_dim = 100
-    num_angles = 360
+    # num_angles = 360
+    num_angles = 1
     BFdockingFFT = TorchDockingFFT(padded_dim=padded_dim, num_angles=num_angles)
     model = SamplingModel(BFdockingFFT, IP=True).to(device=0)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
@@ -48,10 +53,10 @@ if __name__ == '__main__':
     Trainer.run_trainer(
         train_epochs=train_epochs, train_stream=train_stream, valid_stream=None, test_stream=None)
 
-    #### run profiler
-    with profiler.profile(with_stack=True, profile_memory=True) as prof:
-        Trainer.run_trainer(
-            train_epochs=train_epochs, train_stream=train_stream, valid_stream=None, test_stream=None)
-
-    print(prof.key_averages().table(sort_by='self_cpu_time_total'))
-    print(prof.total_average())
+    # #### run profiler
+    # with profiler.profile(with_stack=True, profile_memory=True) as prof:
+    #     Trainer.run_trainer(
+    #         train_epochs=train_epochs, train_stream=train_stream, valid_stream=None, test_stream=None)
+    #
+    # print(prof.key_averages().table(sort_by='self_cpu_time_total'))
+    # print(prof.total_average())
